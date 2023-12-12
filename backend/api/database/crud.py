45065f0333b3b6f1187109196729377e615c07fb
kematin <kematin@inbox.ru>
2023-12-12 22:32:54 +0500

    move db dependency to crud Database class

---
+++ backend/api/database/crud.py
@@ -0,0 +1,40 @@
+from typing import Annotated, Any, List
+
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from database.connection import Base, SessionLocal
+
+
+# dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class Database:
+    def __init__(self, model: Base, db: db_dependency):
+        self.model = model
+        self.db = db
+
+    def get_all(self, skip: int = 0, limit: int = 100) -> List[Any]:
+        return self.db.query(self.model).offset(skip).limit(limit).all()
+
+    def get(self, id: int) -> Any:
+        item = self.db.query(self.model).filter(self.model.id == id).first()
+        if item:
+            return item
+        else:
+            return False
+
+    def create(self, data: dict) -> None:
+        db_data = self.model(**data)
+        self.db.add(db_data)
+        self.db.commit()
+        self.db.refresh(db_data)
--- backend/api/routes/users.py
+++ backend/api/routes/users.py
@@ -1,9 +1,7 @@
-from typing import Annotated
-
 from database.connection import SessionLocal, engine
-from fastapi import APIRouter, Depends, status
+from database.crud import Database
+from fastapi import APIRouter, HTTPException, status
 from models.users import Base, RegisterUser, User
-from sqlalchemy.orm import Session
 
 Base.metadata.create_all(bind=engine)
 
@@ -12,32 +10,35 @@ Base.metadata.create_all(bind=engine)
 def get_db():
     db = SessionLocal()
     try:
-        yield db
+        return db
     finally:
         db.close()
 
 
-db_dependency = Annotated[Session, Depends(get_db)]
-
 users_router = APIRouter(tags=["Users"])
+users_database = Database(User, get_db())
 
 
 @users_router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_user(user: RegisterUser, db: db_dependency):
+async def create_user(user: RegisterUser):
     user = user.dict()
-    db_user = User(**user)
-    db.add(db_user)
-    db.commit()
+    users_database.create(user)
     return {"message": "successfull."}
 
 
 @users_router.get("/")
-async def get_all_users(db: db_dependency):
-    users_list = db.query(User).all()
+async def get_all_users():
+    users_list = users_database.get_all()
     return {"users": users_list}
 
 
 @users_router.get("/{user_id}")
-async def get_user(user_id: int, db: db_dependency):
-    db_user = db.query(User).filter(User.id == user_id).first()
-    return {"user": db_user}
+async def get_user(user_id: int):
+    db_user = users_database.get(user_id)
+    if db_user:
+        return {"user": db_user}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id {user_id} not found.",
+        )
